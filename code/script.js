668636aeff541f2dd3aeb2cdbe4e8a1214981151
alyRady <aliomar.rady@gmail.com>
2020-05-21 13:51:20 +0200

    images

--- code/script.js
+++ code/script.js
@@ -8,13 +8,15 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(startVideo)
 
-function startVideo() {
+ async function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
-}
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  document.body.append('Loaded')
 
 video.addEventListener('play',async () => {
   const canvas = faceapi.createCanvasFromMedia(video)
@@ -25,24 +27,31 @@ video.addEventListener('play',async () => {
     const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    resizedDetections.forEach(detection => {
-      const box = detection.detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' })
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
       drawBox.draw(canvas)
-    });
+    })
     //faceapi.draw.drawDetections(canvas, resizedDetections, { label: 'Face' })
     //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
+}
 
 function loadLabeledImages(){
   const labels = ['Ali','Moha']
   return Promise.all(
     labels.map(async label =>{
+      const descriptions = []
       for (let i = 1; i<=2; i++){
-        const img = await faceapi.fetchImage()
+        const img = await faceapi.fetchImage(`https://github.com/alyRady/FaceRecognition/tree/master/code/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+
       }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
     })
   )
 }
